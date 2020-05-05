52f7e2d8d311f9ce4efc8b557834ee3dd8afad37
yul <yu.li@tecsys.com>
2020-05-05 09:48:57 -0400

    Initial project commit

---
+++ src/test/java/guru/sfg/mssc/beer/service/MsscBeerServiceApplicationIT.java
@@ -0,0 +1,13 @@
+package guru.sfg.mssc.beer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
