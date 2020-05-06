2e22d88a42986f28e49d4f113c2f23cf532bf8d1
yul <yu.li@tecsys.com>
2020-05-05 21:13:37 -0400

    Create web controller for BeerDTo. Closes #3

---
+++ src/main/java/guru/sfg/mssc/beer/service/web/controller/BeerController.java
@@ -0,0 +1,38 @@
+//: guru.sfg.mssc.beer.service.web.controller.BeerController.java
+
+
+package guru.sfg.mssc.beer.service.web.controller;
+
+
+import guru.sfg.mssc.beer.service.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("id") UUID id) {
+        // TODO: Impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beer) {
+        // TODO: Impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateBeerById(
+            @PathVariable("id") UUID id, @RequestBody BeerDto beer) {
+        // TODO: Impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+}///:~
