4cdc8751f9625d6bfbdd0260a052b4b1f48171fd
yul <yu.li@tecsys.com>
2020-05-22 07:32:49 -0400

    Adding error & validation handling to controller. Closes #12

--- src/main/java/guru/sfg/mssc/beer/service/web/controller/BeerController.java
+++ src/main/java/guru/sfg/mssc/beer/service/web/controller/BeerController.java
@@ -7,13 +7,9 @@ package guru.sfg.mssc.beer.service.web.controller;
 import guru.sfg.mssc.beer.service.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 
@@ -42,24 +38,24 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List> validationErrorHandler(
-            MethodArgumentNotValidException e) {
-
-        int size = e.getBindingResult().getAllErrors().size();
-
-        List<String> errors = new ArrayList<>(size);
-
-        e.getBindingResult().getAllErrors().stream()
-                .forEach(oe -> {
-                    String field = ((FieldError) oe).getField();
-                    String errMsg = String.join(" ", field,
-                            oe.getDefaultMessage());
-                    System.out.println(">>>>>>> " + errMsg);
-                    errors.add(errMsg);
-                });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<List> validationErrorHandler(
+//            MethodArgumentNotValidException e) {
+//
+//        int size = e.getBindingResult().getAllErrors().size();
+//
+//        List<String> errors = new ArrayList<>(size);
+//
+//        e.getBindingResult().getAllErrors().stream()
+//                .forEach(oe -> {
+//                    String field = ((FieldError) oe).getField();
+//                    String errMsg = String.join(" ", field,
+//                            oe.getDefaultMessage());
+//                    System.out.println(">>>>>>> " + errMsg);
+//                    errors.add(errMsg);
+//                });
+//
+//        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+//    }
 
 }///:~
